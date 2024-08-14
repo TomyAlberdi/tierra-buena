134ffa6d70628ffcf518c04de4eedf26f8afd6ef
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-08-14 19:07:36 -0300

    Minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import "./App.scss";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Header from "./Pages/Header/Header";
+import { register } from "swiper/element";
 
 function App() {
+
+  useEffect(() => {
+    register()
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+let imgs = [
+  "http://kfkit.rometheme.pro/garvest/wp-content/uploads/sites/21/2022/03/L-6.jpg",
+  "http://kfkit.rometheme.pro/garvest/wp-content/uploads/sites/21/2022/03/L-2.jpg",
+  "http://kfkit.rometheme.pro/garvest/wp-content/uploads/sites/21/2022/03/L-4.jpg",
+  "http://kfkit.rometheme.pro/garvest/wp-content/uploads/sites/21/2022/03/L-5.jpg"
+]
 
 const Header = () => {
   return (
     <div className="Header">
-      header
+      <Swiper
+        slidesPerView="1"
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        speed={1000}
+        loop="true"
+        modules={[Autoplay]}
+        className='mySwiper'
+      >
+        {
+          imgs.map((img, index) => {
+            return (<SwiperSlide key={index} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${img})`}}/>)
+          })
+        }
+      </Swiper>
     </div>
   )
 }
